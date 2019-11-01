0d561fe9dece6359c3e51df426cbf37629b9c06e
dkumar09 <onlydeepender@gmail.com>
2019-11-02 01:09:26 +0530

    Cleaned code and converted into modules

---
+++ eigen.h
@@ -0,0 +1,9 @@
+#ifndef _EIGEN_H
+#define _EIGEN_H
+double pythag ( double a, double b );//Returns hypotenuse of sides a and b
+double r8_abs ( double x );//Returns absolute value of x
+double r8_max ( double x, double y );//Returns max val between x and y
+double r8_min ( double x, double y );//Returns min val between x and y
+double r8_sign ( double x );//returns sign of input
+int tql2 ( int n, double d[], double e[], double z[] );//calculates eigen value
+#endif //_EIGEN_H
