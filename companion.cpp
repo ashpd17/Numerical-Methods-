0ea387e64c054215ddf886daa6bc313318952603
dkumar09 <onlydeepender@gmail.com>
2019-11-03 15:09:14 +0530

    added function input

--- companion.cpp
+++ companion.cpp
@@ -16,23 +16,7 @@ void extrapolate1(double E[],double E2[],int n,double res[])//Richardson Extrapo
         
 	}
 }
-double p_f(double x)
-{
-	double pi = 3.14159265359;
-	double value = -(1-pow(x,2));
-	return 1.0;
-}
-double q_f(double x)
-{
-	double value =(0.25);
-	return 0;
-}
-double w_f(double x)
-{   double pi = 3.14159265359;
-	double value = (64*pow(pi,2)/(9*pow(x,6)));
-	return 1.0;
-}
-void fdm(int n, double a, double b, double alp_1,double alp_2,double bet_1, double bet_2,double P[])//finite difference method1
+void fdm(int n, double a, double b, double alp_1,double alp_2,double bet_1, double bet_2,double P[],double z[],double p_f(double x1),double q_f(double x2),double w_f(double x3))//finite difference method1
 {
 	double x[n+1],e[n], h = (b-a)/n;
 	for(int i = 0; i <= n; i++)
@@ -90,18 +74,18 @@ void fdm(int n, double a, double b, double alp_1,double alp_2,double bet_1, doub
 	}
     //z input identity matrix of order n 
     //Returns Eigen vectors
-    double z[(n)*(n)];
-	for(int i = 0; i < (n)*(n); i++)
-	{
-		if(i%(n+1) == 0)
-		{
-			z[i] = 1;
-		}
-		else
-		{
-			z[i] = 0;
-		}
-	}
+    // double z[(n)*(n)];
+	// for(int i = 0; i < (n)*(n); i++)
+	// {
+	// 	if(i%(n+1) == 0)
+	// 	{
+	// 		z[i] = 1;
+	// 	}
+	// 	else
+	// 	{
+	// 		z[i] = 0;
+	// 	}
+	// }
 	double E[n];
 	E[0] = 0;
 	for(int i = 1; i < n; i++)
--- main.cpp
+++ main.cpp
@@ -9,6 +9,20 @@
 #include"eigen.cpp"//contains tql2 method
 #include"companion.cpp"//contains other functions
 using namespace std;
+double p_f(double x)
+{
+		return 1.0;
+}
+double q_f(double x)
+{
+	double value =(0.25);
+	return 0;
+}
+double w_f(double x)
+{   double pi = 3.14159265359;
+	double value = (64*pow(pi,2)/(9*pow(x,6)));
+	return 1.0;
+}
 ofstream ofs("lambda.csv");
 int main(){
 	cout.precision(10);    
@@ -18,7 +32,7 @@ int main(){
 	double a = 0,b=1.0;
 	double alp_1 = 1, alp_2 = 0, bet_1 = 1, bet_2 = 0;
 	double res[n];
-  /*  double dk[n*n];
+  	double dk[n*n];
     double dk1[n*n];
 	for(int i = 0; i < (n)*(n); i++)
 	{
@@ -32,10 +46,10 @@ int main(){
 			dk[i] = 0;
             dk1[i] = 0;
 		}
-	}*/
-	fdm(n,a,b,alp_1,alp_2,bet_1,bet_2,P);
+	}
+	fdm(n,a,b,alp_1,alp_2,bet_1,bet_2,P,dk,p_f,q_f,w_f);
 	n = n/2;
-	fdm(n,a,b,alp_1,alp_2,bet_1,bet_2,P2);
+	fdm(n,a,b,alp_1,alp_2,bet_1,bet_2,P2,dk1,p_f,q_f,w_f);
 	extrapolate(P,P2,2*n,res);//for richardson extrapolation
 	for(int i = 0; i < 8; i++){
 		cout<<"lambda "<< i+1 <<" = "<<P[i]<<"\t";
@@ -44,14 +58,14 @@ int main(){
 		cout<<"lambda"<<i+1<<" = "<<pow((i+1)*3.14,2)<<endl;
 	}
 	n=n*2;
-    // for(int i=0;i<n;i++){
-	// 	for(int j=i;j<n;j++){
-	// 		//cout<<j/(100.0)<<","<<dk[i*n+j]<<endl;
-	// 		ofs<<j/(100.0)<<","<<dk[i*n+j]<<endl;
-	// 	}
+    for(int i=0;i<n;i++){
+		for(int j=i;j<n;j++){
+			//cout<<j/(100.0)<<","<<dk[i*n+j]<<endl;
+			ofs<<j/(200.0)<<","<<dk[i*n+j]<<endl;
+		}
         
-    // }
-	// ofs.close();
+    }
+	ofs.close();
     system("pause");
     return 0;
 }
